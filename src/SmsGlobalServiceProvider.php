f8dcbe2697233e70cff7d9ca9e09885853ae2b00
joshua <joshua@techbayportal.com>
2020-05-28 09:59:27 +0400

    Initialize project

---
+++ src/SmsGlobalServiceProvider.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace JoshuaChinemezu\SmsGlobal;
+
+use Illuminate\Support\ServiceProvider;
+
+class SmsGlobalServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
