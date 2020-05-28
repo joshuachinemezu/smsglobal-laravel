71c82bdcd35742658d6dd0c0140a11860d5513ac
joshua <joshua@techbayportal.com>
2020-05-28 18:57:37 +0400

    Add package facade

---
+++ src/Facades/SmsGlobal.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the Laravel Rave package.
+ *
+ * (c) Joshua Chinemezu <joshuachinemezu@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace JoshuaChinemezu\SmsGlobal\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class SmsGlobal extends Facade
+{
+    /**
+     * Get the registered name of the component
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'smsglobal-laravel';
+    }
+}
